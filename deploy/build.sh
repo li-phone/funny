e61036656193ad4260eb5a91b876b197611eeb03
li-phone <974122407@qq.com>
2023-01-27 23:58:28 +0800

    update build.sh

--- deploy/build.sh
+++ deploy/build.sh
@@ -4,18 +4,24 @@ NAME="deploy"
 VERSION="1.0.0"
 HASH_CMD="md5 sha1 sha256"
 PREFIX=$NAME-$VERSION-win_x64
+SUFFIX=".exe"
 ICO_PATH="ico/$NAME-$VERSION-48x48.ico"
 
-
 rm -rf build/ rm -rf dist/
+
+os_version=$(uname -a | awk '{print $1}')
+if [ $os_version = "Linux" ]; then
+  SUFFIX=""
+  PREFIX=$NAME-$VERSION-$os_version
+fi
 
 if [ -f $NAME.spec ]; then
   pyinstaller --clean $NAME.spec
   mkdir -p release
-  cp dist/$NAME.exe dist/$PREFIX.exe
-  cp dist/$PREFIX.exe release/
+  cp dist/$NAME${SUFFIX} dist/$PREFIX${SUFFIX}
+  cp dist/$PREFIX${SUFFIX} release/
   for hash in $HASH_CMD; do
-    ${hash}sum dist/$PREFIX.exe | awk '{print $1}' > dist/$PREFIX.${hash}
+    ${hash}sum dist/$PREFIX${SUFFIX} | awk '{print $1}' >dist/$PREFIX.${hash}
     cp -rf dist/$PREFIX.${hash} release/
   done
 else
--- fake-stream/build.sh
+++ fake-stream/build.sh
@@ -5,16 +5,22 @@ NAME="FakeStream"
 VERSION="2.0.0"
 HASH_CMD="md5 sha1 sha256"
 PREFIX=$NAME-$VERSION-win_x64
+SUFFIX=".exe"
 ICO_PATH="ico/$NAME-$VERSION-48x48.ico"
-
 
 rm -rf build/ rm -rf dist/
 
+os_version=$(uname -a | awk '{print $1}')
+if [ $os_version = "Linux" ]; then
+  SUFFIX=""
+  PREFIX=$NAME-$VERSION-$os_version
+fi
+
 pyinstaller --clean -F $NAME/$MAIN.py -i $ICO_PATH -w
 mkdir -p release
-cp dist/$MAIN.exe dist/$PREFIX.exe
-cp dist/$PREFIX.exe release/
+cp dist/$MAIN${SUFFIX} dist/$PREFIX${SUFFIX}
+cp dist/$PREFIX${SUFFIX} release/
 for hash in $HASH_CMD; do
-  ${hash}sum dist/$PREFIX.exe | awk '{print $1}' > dist/$PREFIX.${hash}
+  ${hash}sum dist/$PREFIX${SUFFIX} | awk '{print $1}' >dist/$PREFIX.${hash}
   cp -rf dist/$PREFIX.${hash} release/
 done
